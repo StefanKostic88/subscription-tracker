ef99543fd5b467a9ad65100709978d04dbcb6958
StefanKostic88 <stefankostic88@yahoo.com>
2025-02-14 12:16:29 +0100

    auth 1.0.1

---
+++ src/controlers/auth.controller.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+
+export const signUp = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  next();
+};
+
+export const signIn = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  next();
+};
+
+export const signOut = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  next();
+};
