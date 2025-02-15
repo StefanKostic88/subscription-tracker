82c14074d4ffd105972318d659d434390bc3f339
StefanKostic88 <stefankostic88@yahoo.com>
2025-02-15 10:47:43 +0100

    user data 1.0.2

--- src/base/user/user.data.ts
+++ src/base/user/user.data.ts
@@ -1,5 +1,5 @@
 import SharedBase from "../shared/shared.base";
-import { User, UserCreationAttributes } from "../../models";
+import { User, UserCreationAttributes, UserDocument } from "../../models";
 import mongoose from "mongoose";
 
 class UserData extends SharedBase {
@@ -19,14 +19,24 @@ class UserData extends SharedBase {
   public async createUser(
     user: UserCreationAttributes,
     session: mongoose.mongo.ClientSession
-  ) {
+  ): Promise<UserDocument | undefined> {
     try {
       const users = await User.create([User], { session });
       const user = users[0];
       return user;
     } catch (error) {
       console.error(error);
-      this.generateError(error);
+      throw this.generateError(error);
+    }
+  }
+
+  public async getUserByEmail(email: string): Promise<UserDocument | null> {
+    try {
+      const user = await User.findOne({ email });
+      return user;
+    } catch (error) {
+      console.error(error);
+      throw this.generateError(error);
     }
   }
 }
