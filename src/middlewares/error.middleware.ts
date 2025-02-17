397d6e94ffb008ea711cb793316cc1c798c9b6f5
StefanKostic88 <stefankostic88@yahoo.com>
2025-02-17 17:49:19 +0100

    small fix 1.0.3

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -9,11 +9,12 @@ export const unsupportedRoutes = (
   const errorMessage = `Unsupported route, Cant find route: ${req.originalUrl}`;
   next(new CustomError(errorMessage, 404));
 };
-
 export const errorHandlerMiddleware = (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   err: any,
   req: Request,
   res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ) => {
   res.status(err.statusCode).json({
@@ -23,39 +24,3 @@ export const errorHandlerMiddleware = (
     },
   });
 };
-
-// export const errorMiddleware = (
-//   err: any,
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   try {
-//     let error = { ...err };
-
-//     // if (err.name === "CastError") {
-//     //   const message = "Resource not found";
-//     //   error = new CustomError(message, 404);
-//     // }
-
-//     // if (err.statusCode === 11000) {
-//     //   const message = "Duplicate field value entered";
-//     //   error = new CustomError(message, 400);
-//     // }
-
-//     // if (err.name === "ValidationError") {
-//     //   const messsage = Object.values(err.errors).map(
-//     //     (val) => (val as { message: string }).message
-//     //   );
-
-//     //   error = new CustomError(messsage.join(", "), 400);
-//     // }
-//     // console.log(error);
-//     // res
-//     //   .status(error.statusCode || 500)
-//     //   .json({ success: false, error: error.message });
-//     res.status(error.statusCode || 500).json({ success: false, error: "ERR" });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
